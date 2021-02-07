81b696581d2bd6fd04998dec6cd7c9167f1d750c
sarazafe <sara@blunder.es>
2021-02-07 20:48:43 +0100

    feat: New component AddDeck.js

---
+++ components/AddDeck.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {View, Text} from "react-native";
+
+/**
+ * Component where a new deck is able to be added to the store
+ */
+export const AddDeck = () => {
+	return (
+		<View>
+			<Text>Adding a deck</Text>
+		</View>
+	);
+};
