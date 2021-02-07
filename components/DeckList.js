b26610be1cacd8bac0f37ccef76f8772421e0000
sarazafe <sara@blunder.es>
2021-02-07 19:26:14 +0100

    feat: Show decks in DeckList (initial approach)

--- components/DeckList.js
+++ components/DeckList.js
@@ -1,11 +1,38 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
+import {connect} from 'react-redux';
 import {Text, View} from "react-native";
+import {getDecks} from "../api/api";
+import {receiveDecks} from "../actions";
 
 /**
  * Component that represents the list of decks
  */
-export default class DeckList extends Component {
-	render(){
-		return (<View><Text>List of decks</Text></View>);
+class DeckList extends Component {
+	componentDidMount() {
+		const {receiveDecks} = this.props;
+		getDecks()
+			.then(decks => receiveDecks(decks));
+	}
+
+	render() {
+		const {decks} = this.props;
+		return (<View><Text>{JSON.stringify(decks)}</Text></View>);
 	}
 }
+
+const mapStateToProps = decks => {
+	return {
+		decks
+	};
+}
+
+const mapDispatchToProps = dispatch => {
+	return {
+		receiveDecks: decks => dispatch(receiveDecks(decks))
+	};
+};
+
+export default connect(
+	mapStateToProps,
+	mapDispatchToProps
+)(DeckList)
