5f5a80c37dcd58d2e5327e7cdf9fbd883fb5945b
sarazafe <sara@blunder.es>
2021-02-07 20:51:38 +0100

    chore: Add tab navigation as main navigation
    There are two tabs, one for going to the list of decks;
    the other one, to go to add new deck

--- App.js
+++ App.js
@@ -1,18 +1,23 @@
-import {StatusBar} from 'expo-status-bar';
 import React from 'react';
-import { createStore } from 'redux';
-import { Provider } from 'react-redux';
-import {StyleSheet, View} from 'react-native';
-import DeckList from "./components/DeckList";
+import {createStore} from 'redux';
+import {Provider} from 'react-redux';
+import {StyleSheet} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import {decks as reducer} from './reducers';
+import {AddDeck} from "./components/AddDeck";
+import {Home} from "./components/Home";
 
 export default function App() {
+	const Tab = createBottomTabNavigator();
 	return (
 		<Provider store={createStore(reducer)}>
-		<View style={styles.container}>
-			<DeckList/>
-			<StatusBar style="auto"/>
-		</View>
+			<NavigationContainer style={styles.container}>
+				<Tab.Navigator>
+					<Tab.Screen name="Home" component={Home} />
+					<Tab.Screen name="Add Deck" component={AddDeck} />
+				</Tab.Navigator>
+			</NavigationContainer>
 		</Provider>
 	);
 }
