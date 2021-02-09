9580439354f1206ae565047f4908cbf7ed5656a1
sarazafe <sara@blunder.es>
2021-02-09 21:08:20 +0100

    feat: Add navigation for going to AddCard component

--- components/Home.js
+++ components/Home.js
@@ -1,7 +1,8 @@
 import React from "react";
 import {createStackNavigator} from "@react-navigation/stack";
 import DeckList from "./DeckList";
-import {DeckDetail} from "./DeckDetail";
+import DeckDetail from "./DeckDetail";
+import AddCard from "./AddCard";
 
 /**
  * Component for routing between views on a stack way
@@ -12,6 +13,7 @@ export const Home = () => {
 		<Stack.Navigator>
 			<Stack.Screen name="Home" component={DeckList}/>
 			<Stack.Screen name="Details" component={DeckDetail}/>
+			<Stack.Screen name="Add card" component={AddCard}/>
 		</Stack.Navigator>
 	);
 };
