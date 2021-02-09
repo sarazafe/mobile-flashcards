1a1098ca1a7f04f01ac4fa6853c7f1aa1344dc82
sarazafe <sara@blunder.es>
2021-02-09 21:07:58 +0100

    feat: Add deck info and buttons to DeckDetail component

--- components/DeckDetail.js
+++ components/DeckDetail.js
@@ -1,14 +1,56 @@
-import React from 'react';
-import {View, Text} from "react-native";
+import React, {Component} from 'react';
+import {View} from "react-native";
+import {connect} from 'react-redux';
+import {DeckCard} from "./DeckCard";
+import {Button} from "./Button";
 
 /**
  * Component that shows the details of a deck
- * @param title - the title of the deck
  */
-export const DeckDetail = ({route: {params: {title}}}) => {
-	return (
-		<View>
-			<Text>{title}</Text>
-		</View>
-	);
+class DeckDetail extends Component {
+
+	/**
+	 * Starts the quiz
+	 */
+	startQuiz = () => {
+
+	};
+
+	/**
+	 * Navigates to page that add a new card
+	 * @param title - the title of the deck
+	 */
+	navigateToAddCard = (title) => {
+		const {navigation} = this.props;
+		navigation.navigate('Add card', {
+			title,
+		});
+	};
+
+	render() {
+		const {deck: {title, questions}} = this.props;
+		return (
+			<View>
+				<DeckCard title={title} numberOfCards={questions.length}/>
+
+				<Button style={{padding: 10}} onPress={this.startQuiz} disabled={questions.length === 0}>
+					Start the quiz!
+				</Button>
+				<Button style={{padding: 10}} onPress={() => this.navigateToAddCard(title)}>
+					Add a card
+				</Button>
+			</View>
+		)
+			;
+	}
+}
+
+const mapStateToProps = (decks, {route: {params: {title}}}) => {
+	return {
+		deck: Object.values(decks).find(deck => (deck.title === title)),
+	};
 };
+
+export default connect(
+	mapStateToProps,
+)(DeckDetail)
