102b3b404d1014c228e0f245e27fa757c5331eaa
sarazafe <sara@blunder.es>
2021-02-10 19:50:05 +0100

    feat: Complete the functionality of adding a card in component

--- components/AddCard.js
+++ components/AddCard.js
@@ -1,18 +1,132 @@
 import React, {Component} from 'react';
-import {connect} from "react-redux";
 import {View, Text, TextInput} from "react-native";
+import {Button} from "./Button";
+import {connect} from "react-redux";
+import {updateDeck} from "../actions";
+import {addCartToDeck} from "../api/api";
 
 /**
- * Component that presents a form to be able to add a card to a deck
+ * Component where a new deck is able to be added to the store
  */
 class AddCard extends Component {
-	render(){
+	state = {
+		question: '',
+		answer: '',
+		errorMsg: '',
+	};
+
+	/**
+	 * Updates the question of the state
+	 * @param question - the new value of the question
+	 */
+	updateQuestion = question => {
+		this.setState({
+			question,
+		});
+	};
+
+	/**
+	 * Updates the answer of the state
+	 * @param answer - the new value of the answer
+	 */
+	updateAnswer = answer => {
+		this.setState({
+			answer,
+		});
+	};
+
+	/**
+	 * Resets the state
+	 */
+	resetState = () => {
+		this.setState({
+			question: '',
+			answer: '',
+			errorMsg: '',
+		});
+	};
+
+	/**
+	 * Adds the card to the list of cards of the deck
+	 */
+	addCard = () => {
 		const {title} = this.props;
+		const {question, answer} = this.state;
+		addCartToDeck(title, {
+			question,
+			answer,
+		}).then(() => {
+			this.props.updateDeck({
+				title,
+				question: {
+					question,
+					answer,
+				}
+			});
+
+			this.resetState()
+
+			const {navigation} = this.props;
+			navigation.navigate('Details');
+		}).catch(() => {
+			this.setState({
+				question: '',
+				answer: '',
+				errorMsg: 'Sorry, there was be an error adding the card. Please, try it again.',
+			});
+		});
+	};
+
+	/**
+	 * Renders the error view
+	 */
+	renderErrorView = () => {
+		const {errorMsg} = this.state;
+		return (<View>
+			<Text>{errorMsg}</Text>
+			<Button style={{padding: 10}} onPress={this.resetState}>
+				Try again!
+			</Button>
+		</View>);
+	};
+
+	/**
+	 * Renders the view form
+	 */
+	renderFormView = ()=>{
+		const {title} = this.props;
+		const {question, answer} = this.state;
 		return (
 			<View>
-				<Text>Title - {title}</Text>
+				<Text>Let's add a new card to {title} deck!!</Text>
+				<TextInput
+					style={{height: 40, borderColor: 'gray', borderWidth: 1}}
+					onChangeText={text => this.updateQuestion(text)}
+					placeholder='Write a question'
+					value={question}
+				/>
+				<TextInput
+					style={{height: 40, borderColor: 'gray', borderWidth: 1}}
+					onChangeText={text => this.updateAnswer(text)}
+					placeholder='Write an answer'
+					value={answer}
+				/>
+				<Button style={{padding: 10}} onPress={this.addCard} disabled={!question || !answer}>
+					Add card
+				</Button>
 			</View>
-		)
+		);
+	}
+
+	render() {
+		const {errorMsg} = this.state;
+		return (
+			<View>
+				{
+					errorMsg ? this.renderErrorView() : this.renderFormView()
+				}
+			</View>
+		);
 	}
 }
 
@@ -22,4 +136,13 @@ const mapStateToProps = ({}, {route: {params: {title}}}) => {
 	};
 };
 
-export default connect(mapStateToProps)(AddCard)
+const mapDispatchToProps = dispatch => {
+	return {
+		updateDeck: deck => dispatch(updateDeck(deck))
+	};
+};
+
+export default connect(
+	mapStateToProps,
+	mapDispatchToProps
+)(AddCard)
