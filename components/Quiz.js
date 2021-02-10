bb8bbd742c0f28d4908cc95eb2d19f58368ddb99
sarazafe <sara@blunder.es>
2021-02-10 21:32:30 +0100

    feat: Add quiz component
    Component where the quiz takes place.
    This is the first version without extracting to smaller components and so on.

---
+++ components/Quiz.js
@@ -0,0 +1,158 @@
+import React, {Component} from 'react';
+import {View} from "react-native";
+import {connect} from 'react-redux';
+import {Button} from "./Button";
+import {Text} from "react-native-web";
+import {FontAwesome} from '@expo/vector-icons';
+
+/**
+ * Component where quiz takes place
+ */
+class Quiz extends Component {
+	state = {
+		currentQuestion: {},
+		remainingQuestions: [],
+		totalQuestions: 0,
+		rightQuestions: 0,
+		showQuestion: true,
+	};
+
+	componentDidMount() {
+		const {deck: {questions}} = this.props;
+
+		// Shuffle the questions
+		const shuffledQuestions = [...questions].sort(() => Math.random() - 0.5);
+		const currentQuestion = shuffledQuestions.splice(0, 1)[0];
+		this.setState({
+			currentQuestion: currentQuestion,
+			remainingQuestions: shuffledQuestions,
+			totalQuestions: questions.length,
+		});
+	}
+
+	/**
+	 * Answers the question. Increments the number of right answers and shows the next question
+	 * @param correct - flag to indicate if the chosen answer for the question was right or not
+	 */
+	answerQuestion = correct => {
+		if (correct) {
+			this.setState((currentState) => ({
+				...currentState,
+				rightQuestions: currentState.rightQuestions + 1
+			}));
+		}
+
+		this.showNextQuestion();
+	};
+
+	/**
+	 * Shows next question
+	 */
+	showNextQuestion = () => {
+		this.setState((currentState) => {
+			const {remainingQuestions} = currentState;
+			const questions = [...remainingQuestions];
+			const currentQuestion = questions.splice(0, 1)[0];
+			return {
+				...currentState,
+				currentQuestion,
+				remainingQuestions: questions,
+			}
+		});
+	};
+
+	/**
+	 * Toggle show question flag
+	 */
+	toggleQuestion = () => {
+		this.setState(currentState => ({
+			...currentState,
+			showQuestion: !currentState.showQuestion,
+		}));
+	};
+
+	render() {
+		const {currentQuestion, remainingQuestions, totalQuestions, rightQuestions, showQuestion} = this.state;
+
+		return (
+			<View>
+				{
+					currentQuestion ?
+						(
+							<View>
+								<Text>Let's start the quiz!</Text>
+								<Text>{remainingQuestions.length}/{totalQuestions}</Text>
+								{
+									showQuestion ?
+										(
+											<View>
+												<View>
+													<Text>{currentQuestion.question}</Text>
+												</View>
+												<View>
+													<Button style={{padding: 10}} onPress={this.toggleQuestion}>
+														Show the answer!
+													</Button>
+												</View>
+												<View>
+													<Button style={{padding: 20}}
+													        onPress={() => this.answerQuestion(true)}>
+														<FontAwesome name="thumbs-up" size={24} color="green"/> Right!
+													</Button>
+													<Button style={{padding: 10}}
+													        onPress={() => this.answerQuestion(false)}>
+														<FontAwesome name="thumbs-down" size={24} color="red"/> Wrong
+													</Button>
+												</View>
+											</View>
+										) : (
+											<View>
+												<View>
+													<Text>{currentQuestion.answer}</Text>
+												</View>
+												<View><Button style={{padding: 10}} onPress={this.toggleQuestion}>
+													Back to question
+												</Button>
+												</View>
+											</View>
+										)
+								}
+							</View>
+						)
+						:
+						(
+							<View>
+								<Text>End of quiz!</Text>
+								{
+									(rightQuestions / totalQuestions) * 100 > 50 ?
+										(
+											<View>
+												<Text>🎉 Congratulations!!!</Text>
+												<Text>You got {rightQuestions} of {totalQuestions} questions right!</Text>
+											</View>
+										) :
+										(
+											<View>
+												<Text>So sorry 🙁!!</Text>
+												<Text>You only got {rightQuestions} of {totalQuestions} right.</Text>
+												<Text>Go study and try it again when you're ready!! 💪🏻</Text>
+											</View>
+										)
+								}
+							</View>
+						)
+				}
+			</View>
+		)
+	}
+}
+
+const mapStateToProps = (decks, {route: {params: {title}}}) => {
+	return {
+		deck: Object.values(decks).find(deck => (deck.title === title)),
+	};
+};
+
+export default connect(
+	mapStateToProps,
+)(Quiz)
