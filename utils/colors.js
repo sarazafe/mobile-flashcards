7deee879c1b9091e413649164e2be773f4ec74ba
sarazafe <sara@blunder.es>
2021-02-12 20:43:13 +0100

    feat: File with colors that will be used for the styling

---
+++ utils/colors.js
@@ -0,0 +1,6 @@
+export const DarkBlue = '#001244';
+export const Blue = '#005086';
+export const LightBlue = '#318fb5';
+export const Green = '#b0cac7';
+export const Salmon = '#f7d6bf';
+export const White = '#ffffff';
