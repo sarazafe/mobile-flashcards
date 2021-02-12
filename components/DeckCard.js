88d6ec7cd51209c7af987cc357d4d3170a3cbc11
sarazafe <sara@blunder.es>
2021-02-12 21:09:19 +0100

    feat: Styling for DeckCard

--- components/DeckCard.js
+++ components/DeckCard.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import {View, Text} from "react-native";
+import {View, Text, StyleSheet} from "react-native";
+import {Black, Blue, LightBlue, White} from "../utils/colors";
 
 /**
  * Component that represents a deck card
@@ -8,9 +9,40 @@ import {View, Text} from "react-native";
  */
 export const DeckCard = ({title, numberOfCards}) => {
 	return (
-		<View>
-			<Text>{title}</Text>
-			<Text>{numberOfCards} cards</Text>
+		<View style={styles.container}>
+			<Text style={styles.title}>{title}</Text>
+			<Text style={styles.subTitle}>{numberOfCards} cards</Text>
 		</View>
 	)
 };
+
+const styles = StyleSheet.create({
+	container: {
+		backgroundColor: White,
+		marginLeft: 20,
+		marginRight: 20,
+		marginTop: 10,
+		marginBottom: 10,
+		padding: 30,
+		borderRadius: 10,
+		shadowColor: Black,
+		shadowOffset: {
+			width: 0,
+			height: 2,
+		},
+		shadowOpacity: 0.25,
+		shadowRadius: 3.84,
+		elevation: 4,
+	},
+
+	title: {
+		fontSize: 20,
+		color: Blue,
+		marginBottom: 10,
+	},
+
+	subTitle: {
+		fontSize: 15,
+		color: LightBlue,
+	}
+});
